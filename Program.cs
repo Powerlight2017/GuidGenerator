7045b970412bdfe125f486fb82d0d4fce648b914
Mikhail Evgrafov <mikhail.evgrafov@aspose.com>
2021-06-30 17:11:25 +0500

    Test Nuget publish

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuidGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GUID: "+Guid.NewGuid().ToString());
+        }
+    }
+}
